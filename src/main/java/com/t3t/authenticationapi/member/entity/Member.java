eba25cd6b8be36f05420061ca7f387ac3cf55e03
woody35545 <woody35545@gmail.com>
2024-05-08 10:51:28 +0900

    feat: #26 `Member` - 마지막 로그인 시간 갱신

--- src/main/java/com/t3t/authenticationapi/member/entity/Member.java
+++ src/main/java/com/t3t/authenticationapi/member/entity/Member.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Entity@Table(name = "members")
@@ -18,7 +19,7 @@ public class Member {
     @Id
     @Column(name = "member_id")
     private Long id;
-    @Column(name = "grade_id")
+    @Column(name = "member_grade_id")
     private Integer gradeId;
     @Column(name = "member_name")
     private String name;
@@ -29,7 +30,7 @@ public class Member {
     @Column(name = "member_birthdate")
     private LocalDate birthdate;
     @Column(name = "member_latest_login")
-    private LocalDate latestLogin;
+    private LocalDateTime latestLogin;
     @Column(name = "member_point")
     private Integer point;
     @Column(name = "member_status")
@@ -38,7 +39,7 @@ public class Member {
     private String role;
     @Builder
     public Member(Long id, Integer gradeId, String name, String phone, String email,
-                  LocalDate birthdate, LocalDate latestLogin, Integer point,
+                  LocalDate birthdate, LocalDateTime latestLogin, Integer point,
                   String status, String role) {
         this.id = id;
         this.gradeId = gradeId;
@@ -50,5 +51,13 @@ public class Member {
         this.point = point;
         this.status = status;
         this.role = role;
+    }
+
+    /**
+     * 마지막 로그인 시간을 갱신한다.
+     * @author woody35545(구건모)
+     */
+    public void updateLastLogin() {
+        this.latestLogin = LocalDateTime.now();
     }
 }
